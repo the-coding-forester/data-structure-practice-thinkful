5bbbc4b0d3fe7619660d7982751e620b49c75745
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-12 17:05:25 -0400

    use linked list

---
+++ LinkedList/useLinkedList.js
@@ -0,0 +1,9 @@
+const LinkedList = require('./linkedList');
+
+const linkedList = new LinkedList();
+
+linkedList.insertAtHead('One');
+linkedList.insertAtHead(2);
+
+// eslint-disable-next-line no-console
+console.log(linkedList);
