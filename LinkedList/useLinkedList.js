80f59eaa40ea15bba6768b4eb0471d2303e5c9fd
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-12 23:45:28 -0400

    Updated to use find functionality

--- LinkedList/useLinkedList.js
+++ LinkedList/useLinkedList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const LinkedList = require('./linkedList');
 
 const linkedList = new LinkedList();
@@ -5,5 +6,17 @@ const linkedList = new LinkedList();
 linkedList.insert('One');
 linkedList.insert(2);
 
-// eslint-disable-next-line no-console
+// You can use `find()` to update the value of a node in the list.
+linkedList.find((node) => node.value === 'One').value = 1;
+
 console.log(linkedList);
+
+console.log(
+  'find 2',
+  linkedList.find((node) => node.value === 2),
+);
+
+console.log(
+  'find 3',
+  linkedList.find((node) => node.value === 3),
+);
