ac569806419badd01262a86b115ba8758a54a541
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-12 23:54:56 -0400

    Calls new insert function as test

--- LinkedList/useLinkedList.js
+++ LinkedList/useLinkedList.js
@@ -7,16 +7,8 @@ linkedList.insert('One');
 linkedList.insert(2);
 
 // You can use `find()` to update the value of a node in the list.
-linkedList.find((node) => node.value === 'One').value = 1;
+// linkedList.find((node) => node.value === 'One').value = 1;
+
+linkedList.insert(1.5, (node) => node.value === 'One');
 
 console.log(linkedList);
-
-console.log(
-  'find 2',
-  linkedList.find((node) => node.value === 2),
-);
-
-console.log(
-  'find 3',
-  linkedList.find((node) => node.value === 3),
-);
